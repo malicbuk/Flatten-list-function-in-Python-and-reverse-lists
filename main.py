96fd2f4e5e7eceb6604819fbd31af1080ea4acc8
malicbuk <105162440+malicbuk@users.noreply.github.com>
2022-05-18 17:34:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,27 @@ def reverse_lists(reversen):
 print(reverse_lists([[1, 2], [3, 4], [5, 6, 7]]))
 
 
+def flatten_lists(flatten):
+    straight = []
+    for element in flatten:
+        if type(element) is list:
+            for atom in element:
+                if type(atom) is list:
+                    for small in atom:
+                        if type(small) is list:
+                            for smallest in small:
+                                straight.append(smallest)
+                        else:
+                            straight.append(small)
+                else:
+                    straight.append(atom)
+        else:
+            straight.append(element)
+        return straight
+
+print(flatten_lists([[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5])
+
+
 
 
 
