47e31a10b982e9caa7a77c4e8a70f9113cb9c24e
malicbuk <105162440+malicbuk@users.noreply.github.com>
2022-05-18 17:31:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-def flatten_lists(flatten):
-    straight = []
-    for element in flatten:
+def reverse_lists(reversen):
+    a=[]
+    reversen.reverse()
+    final_list= []
+    for element in reversen:
         if type(element) is list:
-            for atom in element:
-                if type(atom) is list:
-                    for small in atom:
-                        if type(small) is list:
-                            for smallest in small:
-                                straight.append(smallest)
-                        else:
-                            straight.append(small)
-                else:
-                    straight.append(atom)
-        else:
-            straight.append(element)
-    return  straight
+            element.reverse()
 
-print(flatten_lists([[1,'a',['cat'],2],[[[3]],'dog'],4,5]))
+    return reversen
+
+
+print(reverse_lists([[1, 2], [3, 4], [5, 6, 7]]))
 
 
 
